20b8979230b9dc575edfd92a779bec65c44c1e16
Will He <wwh15@duke.edu>
2025-03-28 13:54:35 -0400

    Feature CONFIG-2: Use new exception handling

--- src/main/java/oogasalad/engine/config/JsonConfigParser.java
+++ src/main/java/oogasalad/engine/config/JsonConfigParser.java
@@ -3,6 +3,7 @@ package oogasalad.engine.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import oogasalad.engine.config.api.ConfigParser;
 
 public class JsonConfigParser implements ConfigParser {
 
@@ -12,11 +13,11 @@ public class JsonConfigParser implements ConfigParser {
     this.mapper = new ObjectMapper();
   }
 
-  public ConfigModel loadFromFile(String filepath) throws ConfigParseException {
+  public ConfigModel loadFromFile(String filepath) throws ConfigException {
     try {
       return mapper.readValue(new File(filepath), ConfigModel.class);
     } catch (IOException e) {
-      throw new ConfigParseException("Failed to parse config file: " + filepath, e);
+      throw new ConfigException("Failed to parse config file: " + filepath, e);
     }
   }
 }
