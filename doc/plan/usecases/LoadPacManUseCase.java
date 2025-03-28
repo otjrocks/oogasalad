061fe582904a46af39fcc4b3fc58efa9a3edd255
Jessica Chen <jc939@duke.edu>
2025-03-28 16:47:31 -0400

    Refactor Data Plan: removed tags from entityConfigs

---
+++ doc/plan/usecases/LoadPacManUseCase.java
@@ -0,0 +1,14 @@
+/**
+ * Use Case: CHARACTER-1: Load PacMan Goal: Load Controllable(s) based on predefined control scheme,
+ * appearance, location and other configuration values
+ */
+class DefineInteractionUseCase {
+
+  public static void main(String[] args) {
+    // Step 1: Read character configuration values from JSON
+
+    // Step 2: Initialize Entity based on JSON data
+
+    // Step 3: Add Entity to GameMap
+  }
+}
