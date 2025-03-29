05ccb1fb8600b07dec69bdd8613740abc20db4ef
Owen Jennings <otj4@duke.edu>
2025-03-29 17:57:30 -0400

    CONFIG-5: add additional theme and finalize styles - closes #6 closes

--- src/main/java/oogasalad/engine/config/GameConfig.java
+++ src/main/java/oogasalad/engine/config/GameConfig.java
@@ -7,7 +7,7 @@ package oogasalad.engine.config;
  */
 public class GameConfig {
 
-  public static final int WIDTH = 900;
+  public static final int WIDTH = 1200;
   public static final int HEIGHT = 900;
   public static final int ELEMENT_SPACING = 10;
 
