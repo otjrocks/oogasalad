d3e08f0e076c6c05cfc8826db9b7ab21b1d8e8b9
Owen Jennings <otj4@duke.edu>
2025-03-29 14:33:34 -0400

    CONFIG-4: enable language selection

---
+++ src/main/java/oogasalad/engine/controller/LanguageController.java
@@ -0,0 +1,82 @@
+package oogasalad.engine.controller;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import oogasalad.engine.LoggingManager;
+
+/**
+ * A class to handle the selection and handling of language messages from a language properties
+ * folder.
+ *
+ * @author Owen Jennings
+ */
+public class LanguageController {
+
+  private static final String LANGUAGE_FILE_PATH = "oogasalad.languages.";
+  private static final String DEFAULT_LANGUAGE = "English";
+
+  private static String myCurrentLanguage = DEFAULT_LANGUAGE;
+  private static ResourceBundle myMessages = getLanguageResourceBundle();
+
+  /**
+   * Get the message string from the language resource config file for the provided key. This method
+   * will use the current language for the requested message.
+   *
+   * @param key The key you are looking for
+   * @return The message specified in the language file for the provided key if it exists or a
+   * missing key message
+   */
+  public String getMessage(String key) {
+    try {
+      if (myMessages != null) {
+        return myMessages.getString(key);
+      } else {
+        logMissingMessage("The language file is missing.");
+        return "ERROR: MISSING KEY";
+      }
+    } catch (MissingResourceException e) {
+      // queries key does not exist in language file or trouble finding messages file
+      // return a default string
+      try {
+        // try displaying to user that key is missing
+        // in their preferred language, fallback to english
+        return myMessages.getString("MISSING_KEY");
+      } catch (MissingResourceException e1) {
+        logMissingMessage(e.getMessage());
+        return "ERROR: MISSING KEY";
+      }
+    }
+
+  }
+
+  /**
+   * Get the display name of the current language
+   *
+   * @return The display name of the current language
+   */
+  public String getLanguage() {
+    return myCurrentLanguage;
+  }
+
+  /**
+   * Set the language for the program.
+   *
+   * @param language The string display name of the language you which to switch to
+   */
+  public void setLanguage(String language) {
+    myCurrentLanguage = language;
+    myMessages = getLanguageResourceBundle();
+  }
+
+  private static ResourceBundle getLanguageResourceBundle() {
+    return ResourceBundle.getBundle(
+        LANGUAGE_FILE_PATH + myCurrentLanguage);
+  }
+
+  private static void logMissingMessage(String e) {
+    if (LoggingManager.LOGGER != null) {
+      LoggingManager.LOGGER.warn("Error loading a message: {}", e);
+    }
+  }
+
+}
