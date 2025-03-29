d1fc3e5f0940d80f8958cde909dea817d7ce0fab
Owen Jennings <otj4@duke.edu>
2025-03-29 14:09:40 -0400

    LOGGING-1: added a logging manager class to initialize the log4j2 logger. added the logger configuration into the resources folder

---
+++ src/main/java/oogasalad/engine/LoggingManager.java
@@ -0,0 +1,32 @@
+package oogasalad.engine;
+
+import java.util.Date;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * A class to handle all the Log4j2 logging for this program.
+ *
+ * @author Owen Jennings
+ */
+public class LoggingManager {
+
+  private static final int LENGTH_OF_DIVIDER = 50;
+  public static final Logger LOGGER = LogManager.getLogger(); // The Logger for this program
+
+  /**
+   * Print the starting information of the program. This should be called whenever the program is
+   * restarted.
+   */
+  public static void printStartInfo() {
+    if (LOGGER != null) {
+      logDividerLine();
+      LOGGER.info("Program started at {}", new Date());
+      logDividerLine();
+    }
+  }
+
+  private static void logDividerLine() {
+    LOGGER.info("-".repeat(LENGTH_OF_DIVIDER));
+  }
+}
