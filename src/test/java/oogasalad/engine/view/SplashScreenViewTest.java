021bb2d262e141d6ec85a2e5d51db5d6d4b4ebe0
Owen Jennings <otj4@duke.edu>
2025-03-31 18:02:42 -0400

    Test SPLASH-1: updated the splash screen test to account for new main controller

--- src/test/java/oogasalad/engine/view/SplashScreenViewTest.java
+++ src/test/java/oogasalad/engine/view/SplashScreenViewTest.java
@@ -10,9 +10,12 @@ import oogasalad.engine.config.GameConfig;
 import oogasalad.engine.controller.MainController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import util.DukeApplicationTest;
 import util.TestUtils;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class SplashScreenViewTest extends DukeApplicationTest {
 
   private TestUtils myTestUtils;
@@ -22,9 +25,7 @@ class SplashScreenViewTest extends DukeApplicationTest {
     Group root = new Group();
     Scene scene = new Scene(root, GameConfig.WIDTH, GameConfig.HEIGHT);
     stage.setScene(scene);
-    MainController mainController = new MainController(stage, root);
-    SplashScreenView splashScreenView = new SplashScreenView(mainController);
-    root.getChildren().add(splashScreenView);
+    new MainController(stage, root);
     stage.show();
   }
 
@@ -55,7 +56,6 @@ class SplashScreenViewTest extends DukeApplicationTest {
     clickOn("#languageSelector");
     clickOn(language);
     waitForFxEvents();  // Ensure that all UI events and updates are processed before assertions
-
     // Verify text elements and buttons change on language change
     myTestUtils.verifyText("#splashScreenTitle", getMessage("TITLE"));
     myTestUtils.verifyText("#languageSelector-label", getMessage("LANGUAGE_SELECTOR_TITLE"));
