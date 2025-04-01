3dbe8c2f40430514c2792bda3991e534c06254db
Owen Jennings <otj4@duke.edu>
2025-04-01 01:29:37 -0400

    Fix: remove additional compilation errors introduced by merge conflicts

--- src/main/java/oogasalad/engine/model/entity/BfsEntity.java
+++ src/main/java/oogasalad/engine/model/entity/BfsEntity.java
@@ -19,25 +19,12 @@ public class BfsEntity extends Entity {
   private final List<int[]> myPath;
   private int myCurrentStep;
 
-<<<<<<< HEAD
-  /**
-   * Create a BFS entity.
-   *
-   * @param entityData The data associated with this entity.
-   */
-  public BfsEntity(EntityData entityData) {
-    super(entityData);
-
-    PathFindingStrategy pathFindingStrategy = new BfsPathFindingStrategy();
-    myPath = pathFindingStrategy.getPath(new Grid(20, 20), (int) getEntityData().getInitialX(),
-        (int) getEntityData().getInitialY(), 0, 0);
-=======
   public BfsEntity(EntityData entityData, GameMap gameMap) {
     super(entityData);
 
     PathFindingStrategy pathFindingStrategy = new BfsPathFindingStrategy();
-    myPath = pathFindingStrategy.getPath(gameMap, (int) getEntityData().getInitialX(), (int) getEntityData().getInitialY(), 0, 0);
->>>>>>> da8d72a282c09c378ada4f4fc650ff4a6c29f9fb
+    myPath = pathFindingStrategy.getPath(gameMap, (int) getEntityData().getInitialX(),
+        (int) getEntityData().getInitialY(), 0, 0);
   }
 
   @Override
