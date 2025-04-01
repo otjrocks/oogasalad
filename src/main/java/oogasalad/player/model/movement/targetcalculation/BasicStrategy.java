973a4ef229dab4c105f769b0857fcad93dc3c1e4
Jessica Chen <jc939@duke.edu>
2025-04-01 00:59:28 -0400

    Refactor: added java doc comments to target calculation

--- src/main/java/oogasalad/player/model/movement/targetcalculation/BasicStrategy.java
+++ src/main/java/oogasalad/player/model/movement/targetcalculation/BasicStrategy.java
@@ -1,6 +1,15 @@
 package oogasalad.player.model.movement.targetcalculation;
 
 
+/**
+ * BasicStrategy is a simple implementation of the TargetStrategy interface.
+ * This strategy always returns a target position of (0, 0).
+ * 
+ * <p>It can be used as a default or placeholder strategy when no specific
+ * targeting logic is required. Honestly, probably will be deleted.
+ * 
+ * @author Jessica Chen
+ */
 public class BasicStrategy implements TargetStrategy {
 
   @Override
--- src/main/java/oogasalad/player/model/movement/targetcalculation/TargetStrategy.java
+++ src/main/java/oogasalad/player/model/movement/targetcalculation/TargetStrategy.java
@@ -1,7 +1,23 @@
 package oogasalad.player.model.movement.targetcalculation;
 
-
+/**
+ * Interface representing a strategy for calculating target positions.
+ * Implementations of this interface define how to determine the X and Y
+ * coordinates of a target position.
+ */
 public interface TargetStrategy {
+
+  /**
+   * Calculates and returns the X-coordinate of the target position.
+   *
+   * @return the X-coordinate of the target position
+   */
   int getTargetPositionX();
+
+  /**
+   * Calculates and returns the Y-coordinate of the target position.
+   *
+   * @return the Y-coordinate of the target position
+   */
   int getTargetPositionY();
 }
