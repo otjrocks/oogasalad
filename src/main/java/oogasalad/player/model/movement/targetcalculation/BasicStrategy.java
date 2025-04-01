1bd6494e4d435088e9117ff4439caf4952c39c65
Jessica Chen <jc939@duke.edu>
2025-04-01 00:57:25 -0400

    Bugfix: fixed an issue where target strategy was not being pushed due to gitignore reasons

---
+++ src/main/java/oogasalad/player/model/movement/targetcalculation/BasicStrategy.java
@@ -0,0 +1,15 @@
+package oogasalad.player.model.movement.targetcalculation;
+
+
+public class BasicStrategy implements TargetStrategy {
+
+  @Override
+  public int getTargetPositionX() {
+    return 0;
+  }
+
+  @Override
+  public int getTargetPositionY() {
+    return 0;
+  }
+}
---
+++ src/main/java/oogasalad/player/model/movement/targetcalculation/TargetStrategy.java
@@ -0,0 +1,7 @@
+package oogasalad.player.model.movement.targetcalculation;
+
+
+public interface TargetStrategy {
+  int getTargetPositionX();
+  int getTargetPositionY();
+}
