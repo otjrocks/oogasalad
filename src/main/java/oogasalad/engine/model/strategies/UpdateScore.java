a1b9be95c3c7419cd08edab4a37f57e4d2d090e0
Austin Huang <ash110@duke.edu>
2025-04-01 13:28:22 -0400

    Doc: add Javadoc comments to UpdateScore class

--- src/main/java/oogasalad/engine/model/strategies/UpdateScore.java
+++ src/main/java/oogasalad/engine/model/strategies/UpdateScore.java
@@ -2,15 +2,34 @@ package oogasalad.engine.model.strategies;
 
 import oogasalad.engine.model.entity.Entity;
 
+/**
+ * The {@code UpdateScore} class implements the {@link CollisionStrategy} interface
+ *  to update the game score when a collision occurs. When two entities collide,
+ *  this strategy increments the score by a specified amount.
+ *
+ * @author Austin Huang
+ */
 public class UpdateScore implements CollisionStrategy {
   private int scoreIncrement;
 
+  /**
+   * Constructs an {@code UpdateScore} strategy with a specified score increment.
+   *
+   * @param increment the amount by which the score should be increased on collision
+   */
   public UpdateScore(int increment) {
     this.scoreIncrement = increment;
   }
 
+  /**
+   * Handles a collision between two entities by updating the game score.
+   * This method simulates score updating by printing a message.
+   *
+   * @param entity1 the first entity involved in the collision
+   * @param entity2 the second entity involved in the collision
+   */
   @Override
   public void handleCollision(Entity entity1, Entity entity2) {
-//    GameState.updateScore(scoreIncrement);
+    System.out.println("GameState.updateScore(scoreIncrement)" + scoreIncrement);
   }
 }
