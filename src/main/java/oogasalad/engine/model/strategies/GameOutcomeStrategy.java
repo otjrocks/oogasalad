be68b20c5c7eed6f1a204231e06fa562ad5db883
Austin Huang <ash110@duke.edu>
2025-04-02 20:08:05 -0400

    Feature: add interface methods for GameOutcomeStrategy

--- src/main/java/oogasalad/engine/model/strategies/GameOutcomeStrategy.java
+++ src/main/java/oogasalad/engine/model/strategies/GameOutcomeStrategy.java
@@ -1,10 +1,27 @@
 package oogasalad.engine.model.strategies;
 
+import oogasalad.engine.model.GameState;
+
 /**
- * An interface that defines game outcomes.
+ * A strategy interface that determines if the game has ended and the outcome.
  *
  * @author Austin Huang
  */
 public interface GameOutcomeStrategy {
 
+  /**
+   * Determines whether the game has ended based on the current game state.
+   *
+   * @param gameState the current state of the game
+   * @return true if the game has ended, false otherwise
+   */
+  boolean hasGameEnded(GameState gameState);
+
+  /**
+   * Gets the outcome of the game if it has ended.
+   *
+   * @param gameState the current state of the game
+   * @return a string describing the game outcome
+   */
+  String getGameOutcome(GameState gameState);
 }
