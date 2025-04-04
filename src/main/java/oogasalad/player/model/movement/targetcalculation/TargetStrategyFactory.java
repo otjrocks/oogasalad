295ecb00e07652563d2e912890487fbf86919bb1
Jessica Chen <jc939@duke.edu>
2025-04-04 17:00:33 -0400

    Refactor MODEL-2: changed factory to use reflection and wrote tests for factory

--- src/main/java/oogasalad/player/model/movement/targetcalculation/TargetStrategyFactory.java
+++ src/main/java/oogasalad/player/model/movement/targetcalculation/TargetStrategyFactory.java
@@ -1,5 +1,6 @@
 package oogasalad.player.model.movement.targetcalculation;
 
+import java.lang.reflect.Constructor;
 import java.util.Map;
 import oogasalad.engine.model.EntityPlacement;
 import oogasalad.engine.model.GameMap;
@@ -12,6 +13,9 @@ import oogasalad.player.model.exceptions.TargetStrategyException;
  */
 public class TargetStrategyFactory {
 
+  private static final String STRATEGY_PACKAGE
+      = "oogasalad.player.model.movement.targetcalculation.";
+
   /**
    * Factory method to create a TargetStrategy based on the control type of the given
    * EntityPlacement.
@@ -22,16 +26,32 @@ public class TargetStrategyFactory {
    * @return a TargetStrategy instance corresponding to the control type of the entity
    * @throws TargetStrategyException if the control type of the entity is unknown or unsupported
    */
-  public static TargetStrategy createTargetStrategy(EntityPlacement placement,
-      GameMap gameMap)
+  public static TargetStrategy createTargetStrategy(EntityPlacement placement, GameMap gameMap)
       throws TargetStrategyException {
-    String strategy = placement.getType().getControlType().toLowerCase();
-    return switch (strategy) {
-      case "targetentity" -> new TargetEntityStrategy(gameMap, placement.getType().getStrategyConfig());
-      case "targetaheadofentity" -> new TargetAheadOfEntityStrategy(gameMap, placement.getType().getStrategyConfig());
-      default -> throw new TargetStrategyException("Unknown target strategy");
-    };
+    String controlType = placement.getType().getControlType();
+    String className = STRATEGY_PACKAGE + capitalize(controlType) + "Strategy";
+
+    try {
+      Class<?> clazz = Class.forName(className);
+      Constructor<?> constructor = clazz.getConstructor(GameMap.class, Map.class);
+      return (TargetStrategy) constructor.newInstance(gameMap,
+          placement.getType().getStrategyConfig());
+    } catch (Exception e) {
+      throw new TargetStrategyException(
+          "Failed to create strategy for control type: " + controlType, e);
+    }
   }
 
-
+  // made by chatGPT
+  private static String capitalize(String input) {
+    String[] parts = input.split("(?=[A-Z])|_|\\s+"); // split camelCase, snake_case, or spaces
+    StringBuilder sb = new StringBuilder();
+    for (String part : parts) {
+      if (!part.isEmpty()) {
+        sb.append(part.substring(0, 1).toUpperCase());
+        sb.append(part.substring(1).toLowerCase());
+      }
+    }
+    return sb.toString();
+  }
 }
