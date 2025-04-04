eb8c84a28476b4247e6461e98dda1048d563a5d7
Jessica Chen <jc939@duke.edu>
2025-04-04 13:12:44 -0400

    Test MODEL-2: setup testcases for TargetType class

--- src/test/java/oogasalad/player/model/movement/targetcalculation/TargetTypeStrategyTest.java
+++ src/test/java/oogasalad/player/model/movement/targetcalculation/TargetTypeStrategyTest.java
@@ -1,4 +1,36 @@
 package oogasalad.player.model.movement.targetcalculation;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+import oogasalad.engine.model.GameMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class TargetTypeStrategyTest {
+
+  private GameMap mockMap;
+
+  @BeforeEach
+  public void setUp() {
+    mockMap = mock(GameMap.class);
+  }
+
+  // tests for get Target Position once if it exists, once if it does not
+  @Test
+  void getTargetPosition_noTilesAheadValidEntity_returnsEntityLocation() {
+  }
+
+  @Test
+  void getTargetPosition_tilesAheadValidEntityWithDirection_returnsEntityLocationNAhead() {}
+
+  @Test
+  void getTargetPosition_tilesAheadValidEntityWithoutDirection_returnsEntityLocationNTilesAbove() {}
+
+  @DisplayName("Current default behavior is to target 0, 0")
+  @Test
+  void getTargetPosition_NoValidEntity_returnsDefaultLocation() {}
+
+
 }
