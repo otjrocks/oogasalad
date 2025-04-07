637f82fbcd46c578b8edfe1e47b2d81876e7ad57
Owen Jennings <otj4@duke.edu>
2025-04-07 16:33:51 -0400

    Test: updated broken tests to use record initialization instead of setter methods

--- src/test/java/oogasalad/engine/model/EntityPlacementTest.java
+++ src/test/java/oogasalad/engine/model/EntityPlacementTest.java
@@ -7,71 +7,64 @@ import org.junit.jupiter.api.Test;
 
 public class EntityPlacementTest {
 
-    private EntityType testEntityType;
-    private EntityPlacement testPlacement;
+  private EntityType testEntityType;
+  private EntityPlacement testPlacement;
 
-    @BeforeEach
-    public void setUp() {
-        testEntityType = new EntityType();
-        testEntityType.setType("Enemy");
-        testEntityType.setControlType("AI");
-        testEntityType.setEffect("Damage");
+  @BeforeEach
+  public void setUp() {
+    testEntityType = new EntityType("Enemy", "AI", "Damage", null, null, null);
+    testPlacement = new EntityPlacement(testEntityType, 30.0, 40.0, "Default");
+  }
 
-        testPlacement = new EntityPlacement(testEntityType, 30.0, 40.0, "Default");
-    }
+  @Test
+  public void testConstructor() {
+    assertEquals(testEntityType, testPlacement.getType());
+    assertEquals(30.0, testPlacement.getX());
+    assertEquals(40.0, testPlacement.getY());
+  }
 
-    @Test
-    public void testConstructor() {
-        assertEquals(testEntityType, testPlacement.getType());
-        assertEquals(30.0, testPlacement.getX());
-        assertEquals(40.0, testPlacement.getY());
-    }
+  @Test
+  public void testGetSetEntityData() {
+    EntityType newEntityType = new EntityType("Player", "AI", "Damage", null, null, null);
+    testPlacement.setResolvedEntityType(newEntityType);
+    assertEquals(newEntityType, testPlacement.getType());
+    assertEquals("Player", testPlacement.getType().type());
+  }
 
-    @Test
-    public void testGetSetEntityData() {
-        EntityType newEntityType = new EntityType();
-        newEntityType.setType("Player");
+  @Test
+  public void testGetSetX() {
+    testPlacement.setX(50.0);
 
-        testPlacement.setResolvedEntityType(newEntityType);
+    assertEquals(50.0, testPlacement.getX());
+  }
 
-        assertEquals(newEntityType, testPlacement.getType());
-        assertEquals("Player", testPlacement.getType().type());
-    }
+  @Test
+  public void testGetSetY() {
+    testPlacement.setY(60.0);
 
-    @Test
-    public void testGetSetX() {
-        testPlacement.setX(50.0);
+    assertEquals(60.0, testPlacement.getY());
+  }
 
-        assertEquals(50.0, testPlacement.getX());
-    }
+  @Test
+  public void testMoveTo() {
+    testPlacement.moveTo(70.0, 80.0);
 
-    @Test
-    public void testGetSetY() {
-        testPlacement.setY(60.0);
+    assertEquals(70.0, testPlacement.getX());
+    assertEquals(80.0, testPlacement.getY());
+  }
 
-        assertEquals(60.0, testPlacement.getY());
-    }
+  @Test
+  public void testToString() {
+    String expected = "EntityPlacement{entityData=Enemy, x=30.0, y=40.0}";
+    assertEquals(expected, testPlacement.toString());
+  }
 
-    @Test
-    public void testMoveTo() {
-        testPlacement.moveTo(70.0, 80.0);
+  @Test
+  public void testNullEntityData() {
+    Exception exception = assertThrows(NullPointerException.class, () -> {
+      new EntityPlacement(null, 10.0, 20.0, "Default").toString();
+    });
 
-        assertEquals(70.0, testPlacement.getX());
-        assertEquals(80.0, testPlacement.getY());
-    }
-
-    @Test
-    public void testToString() {
-        String expected = "EntityPlacement{entityData=Enemy, x=30.0, y=40.0}";
-        assertEquals(expected, testPlacement.toString());
-    }
-
-    @Test
-    public void testNullEntityData() {
-        Exception exception = assertThrows(NullPointerException.class, () -> {
-            new EntityPlacement(null, 10.0, 20.0, "Default").toString();
-        });
-
-        assertNotNull(exception);
-    }
+    assertNotNull(exception);
+  }
 }
