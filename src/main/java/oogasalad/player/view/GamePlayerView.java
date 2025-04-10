a23f98fdc4cdb805bfde4ca7a4e77d974af1af80
Austin Huang <ash110@duke.edu>
2025-04-10 02:06:03 -0400

    Reverted changes by Owen in GameMapController, GameMapView, GamePlayerView, GameView

--- src/main/java/oogasalad/player/view/GamePlayerView.java
+++ src/main/java/oogasalad/player/view/GamePlayerView.java
@@ -67,7 +67,7 @@ public class GamePlayerView extends StackPane {
     }
 
     if (gameMap != null) {
-      myGameView = new GameView(new GameContext(gameMap, myGameState), configModel);
+      myGameView = new GameView(new GameContext(gameMap, myGameState));
       this.getChildren().add(myGameView);
     }
   }
