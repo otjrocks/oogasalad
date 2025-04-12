b50f4afef5efe63514bc1bc5520292bffaab3aea
Jessica Chen <jc939@duke.edu>
2025-04-12 16:04:19 -0400

    Test ENGINE-ENTITY-REFACTOR-1: add test for ControlStrategies

--- src/test/java/oogasalad/player/model/control/NoneControlStrategyTest.java
+++ src/test/java/oogasalad/player/model/control/NoneControlStrategyTest.java
@@ -1,5 +1,25 @@
 package oogasalad.player.model.control;
 
+import static org.mockito.Mockito.*;
+
+import oogasalad.engine.model.entity.Entity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class NoneControlStrategyTest {
 
+  private Entity entity;
+  private ControlStrategy strategy;
+
+  @BeforeEach
+  void setup() {
+    entity = mock(Entity.class);
+    strategy = new NoneControlStrategy();
+  }
+
+  @Test
+  void update_doesNothing_noInteractionWithEntity() {
+    strategy.update(entity);
+    verifyNoInteractions(entity);
+  }
 }
