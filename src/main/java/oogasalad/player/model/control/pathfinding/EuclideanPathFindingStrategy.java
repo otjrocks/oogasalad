45afee27f796b9a4c2c3eb943d6d4bc8b6e82eb0
Jessica Chen <jc939@duke.edu>
2025-04-13 10:08:59 -0400

    Refactor ENGINE-ENTITY-REFACTOR-2: added javadocs

--- src/main/java/oogasalad/player/model/control/pathfinding/EuclideanPathFindingStrategy.java
+++ src/main/java/oogasalad/player/model/control/pathfinding/EuclideanPathFindingStrategy.java
@@ -1,5 +1,9 @@
 package oogasalad.player.model.control.pathfinding;
 
+/**
+ * A strategy for pathfinding that calculates the direction to move in order to minimize
+ * the Euclidean distance to a specified target.
+ */
 public class EuclideanPathFindingStrategy {
 
 }
--- src/main/java/oogasalad/player/model/control/targetcalculation/TargetLocationStrategy.java
+++ src/main/java/oogasalad/player/model/control/targetcalculation/TargetLocationStrategy.java
@@ -1,5 +1,9 @@
 package oogasalad.player.model.control.targetcalculation;
 
+/**
+ * A strategy for determining the target location based on the configuration.
+ * This class is responsible for setting the location to one specified in the configuration.
+ */
 public class TargetLocationStrategy {
 
 }
