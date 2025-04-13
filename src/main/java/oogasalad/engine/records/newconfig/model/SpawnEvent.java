01d230a80e3f615ea7cd1c348d277e3b194953c8
Will He <wwh15@duke.edu>
2025-04-13 19:20:44 -0400

    Feature ENGINE-CONFIG-2: javadoc

--- src/main/java/oogasalad/engine/records/newconfig/model/SpawnEvent.java
+++ src/main/java/oogasalad/engine/records/newconfig/model/SpawnEvent.java
@@ -7,13 +7,12 @@ import oogasalad.engine.model.EntityType;
  * Represents an entity to spawn during gameplay based on specific conditions.
  *
  * @param entityType       the type of entity to spawn (e.g., "cherry", "redghost")
- * @param spawnCondition   condition under which the entity should appear (e.g., "TimeElapsed(30)")
+ * @param spawnCondition   condition under which the entity should appear
  * @param x                x-position on the grid
  * @param y                y-position on the grid
  * @param mode             integer representing the initial behavior mode
- * @param despawnCondition condition under which the entity should be removed (e.g.,
- *                         "TimeElapsed(60)")
- * @author Owen Jennings
+ * @param despawnCondition condition under which the entity should be removed
+ * @author Owen Jennings, Will He
  */
 public record SpawnEvent(EntityType entityType,
                          Condition spawnCondition,
