416fd169f8ab20b381def39a26af00ccf977bae1
troy.ludwig <tdl21@duke.edu>
2025-04-16 22:54:52 -0400

    Feature: PacMan is able to consume frightened ghosts but still loses a life

--- src/main/java/oogasalad/player/controller/GameMapController.java
+++ src/main/java/oogasalad/player/controller/GameMapController.java
@@ -96,7 +96,7 @@ public class GameMapController {
   }
 
   private void applyEntityBCollisionStrategy(Entity e1, Entity e2, CollisionRule collisionRule) {
-    if (checkEntityTypesMatch(e1, e2, collisionRule)) {
+    if (checkEntityTypesAndModesMatch(e1, e2, collisionRule)) {
       for (CollisionEvent eventB : collisionRule.getEventsB()) {
         createAndApplyCollisionStrategy(e1, e2, eventB);
       }
@@ -104,7 +104,7 @@ public class GameMapController {
   }
 
   private void applyEntityACollisionStrategy(Entity e1, Entity e2, CollisionRule collisionRule) {
-    if (checkEntityTypesMatch(e1, e2, collisionRule)) {
+    if (checkEntityTypesAndModesMatch(e1, e2, collisionRule)) {
       for (CollisionEvent eventA : collisionRule.getEventsA()) {
         createAndApplyCollisionStrategy(e1, e2, eventA);
       }
@@ -122,17 +122,19 @@ public class GameMapController {
     }
   }
 
-  private static boolean checkEntityTypesMatch(Entity e1, Entity e2, CollisionRule collisionRule) {
+  private static boolean checkEntityTypesAndModesMatch(Entity e1, Entity e2, CollisionRule collisionRule) {
     return checkCollisionRuleEntityAMatches(e1, collisionRule) && checkCollisionRuleEntityBMatches(
         e2, collisionRule);
   }
 
   private static boolean checkCollisionRuleEntityBMatches(Entity e2, CollisionRule collisionRule) {
-    return collisionRule.getEntityB().equals(e2.getEntityPlacement().getType().type());
+    return collisionRule.getEntityB().equals(e2.getEntityPlacement().getType().type()) &&
+            collisionRule.getModeB().equals(e2.getEntityPlacement().getMode());
   }
 
   private static boolean checkCollisionRuleEntityAMatches(Entity e1, CollisionRule collisionRule) {
-    return collisionRule.getEntityA().equals(e1.getEntityPlacement().getType().type());
+    return collisionRule.getEntityA().equals(e1.getEntityPlacement().getType().type()) &&
+            collisionRule.getModeA().equals(e1.getEntityPlacement().getMode());
   }
 
   private List<List<Entity>> getAllCollisions() {
