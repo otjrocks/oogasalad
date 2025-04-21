30d411eb0b34a8a21027c047e104df1bfade9313
Owen Jennings <otj4@duke.edu>
2025-04-20 20:00:25 -0400

    Refactor DESIGN-16: removed sources of dead/unused code from game state object and their associated methods

--- src/main/java/oogasalad/player/model/GameState.java
+++ src/main/java/oogasalad/player/model/GameState.java
@@ -1,13 +1,5 @@
 package oogasalad.player.model;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Implementation of the GameState interface. This class manages the player's score, lives, and HUD
  * components. It also provides functionality for saving and loading game states.
@@ -19,7 +11,6 @@ public class GameState implements GameStateInterface {
   private int score;
   private int lives;
   private boolean gameOver;
-  private final List<HudComponent> hudComponents;
   private double timeElapsed = 0;
 
   /**
@@ -31,7 +22,6 @@ public class GameState implements GameStateInterface {
     this.score = 0;
     this.lives = initialLives;
     this.gameOver = false;
-    this.hudComponents = new ArrayList<>();
   }
 
   @Override
@@ -54,45 +44,10 @@ public class GameState implements GameStateInterface {
     return lives;
   }
 
-  // Asked ChatGPT to help with this method
-  @Override
-  public void saveState(String filePath) throws SaveFailedException {
-    try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))) {
-      out.writeObject(this);
-    } catch (IOException e) {
-      throw new SaveFailedException("Failed to save game state: " + e.getMessage());
-    }
-  }
-
-  // Asked ChatGPT to help with this method
-  @Override
-  public void loadState(String filePath) throws LoadFailedException {
-    try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath))) {
-      GameState loadedState = (GameState) in.readObject();
-      this.score = loadedState.score;
-      this.lives = loadedState.lives;
-      this.hudComponents.clear();
-      this.hudComponents.addAll(loadedState.hudComponents);
-    } catch (IOException | ClassNotFoundException e) {
-      throw new LoadFailedException("Failed to load game state: " + e.getMessage());
-    }
-  }
-
-  @Override
-  public void addHudComponent(HudComponent component) {
-    hudComponents.add(component);
-  }
-
-  @Override
-  public List<HudComponent> getHudComponents() {
-    return new ArrayList<>(hudComponents);
-  }
-
   @Override
   public void resetState() {
     this.score = 0;
     this.lives = 0;
-    this.hudComponents.clear();
   }
 
   @Override
@@ -115,9 +70,5 @@ public class GameState implements GameStateInterface {
     this.timeElapsed = 0;
   }
 
-  @Override
-  public boolean isGameOver() {
-    return gameOver;
-  }
 }
 
