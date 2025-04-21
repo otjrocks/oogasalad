6232dda05a3523ef71f1931d547b1530e3d0554d
troy.ludwig <tdl21@duke.edu>
2025-04-21 11:26:24 -0400

    Fix PLAYER-MODE-11: Changed structure of record to allow for transition mode

--- src/main/java/oogasalad/engine/records/config/model/collisionevent/TemporaryModeChangeCollisionEventRecord.java
+++ src/main/java/oogasalad/engine/records/config/model/collisionevent/TemporaryModeChangeCollisionEventRecord.java
@@ -2,5 +2,5 @@ package oogasalad.engine.records.config.model.collisionevent;
 
 import oogasalad.engine.records.config.model.CollisionEventInterface;
 
-public record TemporaryModeChangeCollisionEventRecord(String entityType, String temporaryMode, int duration) implements CollisionEventInterface {
+public record TemporaryModeChangeCollisionEventRecord(String entityType, String temporaryMode, String transitionMode, int duration, int transitionTime) implements CollisionEventInterface {
 }
