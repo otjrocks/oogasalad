2cb3e2a21474da81f6322be3fe4751dbc16f4b43
Owen Jennings <otj4@duke.edu>
2025-04-27 23:50:45 -0400

    Doc DESIGN-18: adddocumentation for player api

--- src/main/java/oogasalad/player/controller/api/package-info.java
+++ src/main/java/oogasalad/player/controller/api/package-info.java
@@ -1,1 +1,15 @@
 package oogasalad.player.controller.api;
+
+/**
+ * GameEndHandlerInterface
+ * <h1>
+ * Overview:
+ * </h1>
+ * <p>
+ * A functional interface for handling game end events, such as a player winning, losing, or
+ * reaching a victory condition.
+ *
+ * @author Austin Huang
+ * @version 1.0
+ * @since 1.0
+ */
