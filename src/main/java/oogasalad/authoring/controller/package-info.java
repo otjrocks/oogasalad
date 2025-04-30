2213fec344be7570d5c498cd9758c61fd1e9c9c4
Will He <wwh15@duke.edu>
2025-04-29 22:42:41 -0400

    Doc: Package-info comments

--- src/main/java/oogasalad/authoring/controller/package-info.java
+++ src/main/java/oogasalad/authoring/controller/package-info.java
@@ -1,1 +1,16 @@
+/**
+ * Contains the controllers that coordinate interactions between the authoring UI and the underlying data models.
+ * <p>
+ * These classes manage user input, update the AuthoringModel in response to changes, and handle events such as
+ * saving, loading, or modifying game configuration files. Controllers serve as the bridge between UI components
+ * and the game's data representation.
+ * <p>
+ * Responsibilities include:
+ * <ul>
+ *   <li>Managing communication between views and the AuthoringModel</li>
+ *   <li>Responding to user actions (e.g., button clicks, dropdown changes)</li>
+ *   <li>Triggering serialization/deserialization processes</li>
+ *   <li>Validating and applying configuration changes</li>
+ * </ul>
+ */
 package oogasalad.authoring.controller;
